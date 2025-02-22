ee79dd53bb48a6d6210f65333119ad53c14d002a
Gabriel Hewitt <97916103+ghewitt95@users.noreply.github.com>
2025-02-22 20:51:14 +0000

    praticing comparisons more

--- blankspace.rb
+++ blankspace.rb
@@ -1,21 +1,11 @@
+puts "a".ord
 
-x = "Test"
-y = "String"
+puts "A".ord
 
-puts "Success!" if x + y == "TestString"
+puts "b".ord
 
+puts "B".ord
 
-a = 2 + 3
-b = 4 + 1
+puts "c".ord
 
-puts "Correct!" * 5 if a + b == 10
-
-g = 10 + 10
-j = 10 + 20
-h = 5 + 5
-
-puts "Correct" if g + j == 50
-
-puts "x" > "y"
-
-puts "y" > "x"
+puts "C".ord
