24d1348c9bf0ef2d1b76139ced46401fcf10a386
Gabriel Hewitt <97916103+ghewitt95@users.noreply.github.com>
2025-02-22 20:48:58 +0000

    practice comparing number values with ASCII

--- blankspace.rb
+++ blankspace.rb
@@ -1,9 +1,9 @@
 
-
 x = "Test"
 y = "String"
 
 puts "Success!" if x + y == "TestString"
+
 
 a = 2 + 3
 b = 4 + 1
@@ -15,3 +15,7 @@ j = 10 + 20
 h = 5 + 5
 
 puts "Correct" if g + j == 50
+
+puts "x" > "y"
+
+puts "y" > "x"
