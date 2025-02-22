7d75359f9b2141b6932d56a2c72892abcb8b0fb8
Gabriel Hewitt <97916103+ghewitt95@users.noreply.github.com>
2025-02-22 20:44:57 +0000

    taking notes

--- blankspace.rb
+++ blankspace.rb
@@ -1,1 +1,17 @@
 
+
+x = "Test"
+y = "String"
+
+puts "Success!" if x + y == "TestString"
+
+a = 2 + 3
+b = 4 + 1
+
+puts "Correct!" * 5 if a + b == 10
+
+g = 10 + 10
+j = 10 + 20
+h = 5 + 5
+
+puts "Correct" if g + j == 50
