87c9228b4d50f9b96b7b3499c667d66e16313750
Gabriel Hewitt <97916103+ghewitt95@users.noreply.github.com>
2025-09-12 18:17:48 -0500

    did some practices

--- blankspace.rb
+++ blankspace.rb
@@ -1,7 +1,29 @@
-class Person
-    attr_accessor :first_name
+require "date"
+
+class User
+  attr_accessor :username
+	attr_accessor :email
+  attr_accessor :bio
+  attr_accessor :birthdate
+  
+  def age
+    dob = Date.parse(self.birthdate)
+    now = Date.today
+    age_in_days = now - dob
+    age_in_years = age_in_days / 365
+    
+    return age_in_years.to_i
+  end
+  
+  def about_me
+    return self.username.downcase + " " + self.age.to_s + ": " + self.bio + ". Reach me at: " + self.email
+  end
 end
 
-rb = Person.new
+new_user = User.new
+new_user.username = "JOE"
+new_user.email = "joe@example.com"
+new_user.bio = "Ruby enthusiast"
+new_user.birthdate = "1987-04-19"
 
-pp rb.last_name
+pp new_user.about_me
